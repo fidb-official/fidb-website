bcd10fe44b45af741775e8c835054c4500b8c965
Xie Yuheng <xyheme@gmail.com>
2023-02-06 23:40:13 +0800

    `Homepage` -- design

--- TODO.md
+++ TODO.md
@@ -1,7 +1,3 @@
-# home
-
-`Homepage` -- design
-
 # fidb-manager
 
 [question] can we implement `fidb-manager` in this webwite to load database from localhost?
--- src/pages/homepage/Homepage.vue
+++ src/pages/homepage/Homepage.vue
@@ -4,18 +4,29 @@ import { useRoute, useRouter } from 'vue-router'
 import PageLayout from '../../layouts/page-layout/PageLayout.vue'
 import { HomepageState as State } from './HomepageState'
 
-defineProps<{ encoded: String }>()
-
 const router = useRouter()
 const route = useRoute()
 
-const state = reactive(new State()) as State
+const state = reactive(new State())
 </script>
 
 <template>
   <PageLayout>
-    <div class="flex h-full flex-col">
-      What if we use file system as database?
+    <div class="flex h-full flex-col items-center justify-center">
+      <div class="flex flex-col px-3">
+        <div class="text-6xl font-bold">FiDB</div>
+        <div>What if we use file system as database?</div>
+        <div>
+          <a
+            class="underline decoration-2"
+            href="https://github.com/xieyuheng/fidb"
+            target="_blank"
+          >
+            Let's find out.
+          </a>
+        </div>
+        <div class="py-16"></div>
+      </div>
     </div>
   </PageLayout>
 </template>
