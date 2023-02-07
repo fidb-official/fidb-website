ccc10696834577584360b8eac13a87f44aac5a65
Xie Yuheng <xyheme@gmail.com>
2023-02-08 04:04:07 +0800

    [manager] `stateUpdateDataset`

--- src/pages/manager/stateReactive.ts
+++ src/pages/manager/stateReactive.ts
@@ -1,10 +1,11 @@
 import { reactive } from 'vue'
 import { State } from './State'
+import { stateUpdateDataset } from './stateUpdateDataset'
 
 export function stateReactive(inputState: State): State {
   const state = reactive<State>(inputState)
 
-  //
+  stateUpdateDataset(state)
 
   return state
 }
---
+++ src/pages/manager/stateUpdateDataset.ts
@@ -0,0 +1,27 @@
+import qs from 'qs'
+import { watch } from 'vue'
+import { State } from './State'
+
+export function stateUpdateDataset(state: State): void {
+  watch(
+    () => state.currentDirectory,
+    async (to, from) => {
+      if (to === undefined) {
+        return
+      }
+
+      const response = await fetch(
+        `${state.url}/${to}?${qs.stringify({
+          page: 1,
+          size: 50,
+          properties: {},
+        })}`,
+      )
+
+      const { results } = await response.json()
+
+      state.dataset = results
+    },
+    { immediate: true },
+  )
+}
