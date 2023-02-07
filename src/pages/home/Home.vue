b32a0f8f8ba574297925d09e2c3a3efde68b9871
Xie Yuheng <xyheme@gmail.com>
2023-02-07 16:13:32 +0800

    up

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { reactive } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
+import Lang from '../../components/Lang.vue'
 import PageLayout from '../../layouts/page-layout/PageLayout.vue'
 import { HomeState as State } from './HomeState'
 
@@ -15,14 +16,21 @@ const state = reactive(new State())
     <div class="flex h-full flex-col items-center justify-center">
       <div class="flex flex-col space-y-2 px-3">
         <div class="text-6xl font-bold">FiDB</div>
-        <div>What if we use file system as database?</div>
+        <Lang>
+          <template #zh> 如果用文件系统当数据库会怎样？ </template>
+          <template #en> What if we use file system as database? </template>
+        </Lang>
+
         <div>
           <a
             class="underline decoration-2"
             href="https://github.com/xieyuheng/fidb"
             target="_blank"
           >
-            Let's find out.
+            <Lang>
+              <template #zh> 来试试看！ </template>
+              <template #en> Let's find out! </template>
+            </Lang>
           </a>
         </div>
         <div class="py-16"></div>
