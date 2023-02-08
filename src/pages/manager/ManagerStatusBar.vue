b76bc69370514c01577bb62b71b5ba12818acc81
Xie Yuheng <xyheme@gmail.com>
2023-02-09 05:27:29 +0800

    up

--- src/pages/manager/ManagerStatusBar.vue
+++ src/pages/manager/ManagerStatusBar.vue
@@ -8,11 +8,12 @@ defineProps<{ state: State }>()
   <div
     class="flex items-center justify-between overflow-auto px-1 text-sm font-bold"
     :class="[
-      state.status === 'ok' && 'border-x border-black bg-black text-white',
+      'transition duration-150',
+      'border-x',
+      state.status === 'ok' && 'border-black bg-black text-white',
       state.status === 'running' &&
-        'border-x border-yellow-500 bg-yellow-500 text-yellow-100',
-      state.status === 'error' &&
-        'border-x border-red-500 bg-red-500 text-red-100',
+        'border-yellow-500 bg-yellow-500 text-yellow-100',
+      state.status === 'error' && ' border-red-500 bg-red-500 text-red-100',
     ]"
   >
     <div></div>
