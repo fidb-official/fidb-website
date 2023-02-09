6d64006260925c748555a8d6a3dcbf53afc2bbdf
Xie Yuheng <xyheme@gmail.com>
2023-02-09 21:46:52 +0800

    up

--- src/pages/manager/ManagerDirectoryList.vue
+++ src/pages/manager/ManagerDirectoryList.vue
@@ -2,6 +2,7 @@
 import Lang from '../../components/Lang.vue'
 import { State } from './State'
 import { stateCreateDirectory } from './stateCreateDirectory'
+// import { stateDeleteDirectory } from './stateDeleteDirectory'
 
 defineProps<{ state: State }>()
 
@@ -14,12 +15,16 @@ async function createDirectory(state: State) {
   await stateCreateDirectory(state, directory)
   state.currentDirectory = directory
 }
+
+async function deleteDirectory(state: State) {
+  //
+}
 </script>
 
 <template>
   <div class="flex h-full flex-col border-l border-black">
-    <div class="flex w-full justify-between border-b border-black font-bold">
-      <div class="px-2">
+    <div class="flex w-full justify-between border-b border-black">
+      <div class="px-2 font-bold">
         <Lang>
           <template #zh> 文件夹： </template>
           <template #en> Directories: </template>
@@ -27,31 +32,48 @@ async function createDirectory(state: State) {
       </div>
 
       <button
-        class="px-2 hover:ring-2 hover:ring-inset hover:ring-black"
+        class="px-2 font-bold hover:ring-2 hover:ring-inset hover:ring-black"
         @click="createDirectory(state)"
       >
-        <Lang>
-          <template #zh> + </template>
-          <template #en> + </template>
-        </Lang>
+        +
       </button>
     </div>
 
     <div v-for="directory of state.directories" :key="directory">
-      <button
+      <div
         v-if="directory === state.currentDirectory"
-        class="w-full bg-black px-2 text-left text-white"
-        @click="state.currentDirectory = directory"
+        class="flex justify-between"
       >
-        {{ directory }}
-      </button>
-      <button
-        v-else
-        class="w-full px-2 text-left hover:bg-stone-200"
-        @click="state.currentDirectory = directory"
-      >
-        {{ directory }}
-      </button>
+        <button
+          class="w-full bg-black px-2 text-left text-white"
+          @click="state.currentDirectory = directory"
+        >
+          {{ directory }}
+        </button>
+
+        <button
+          class="bg-black px-2 font-bold text-white hover:ring-2 hover:ring-inset hover:ring-white"
+          @click="deleteDirectory(state)"
+        >
+          <span class="px-0.5"> - </span>
+        </button>
+      </div>
+
+      <div v-else class="flex justify-between hover:bg-stone-200">
+        <button
+          class="w-full px-2 text-left"
+          @click="state.currentDirectory = directory"
+        >
+          {{ directory }}
+        </button>
+
+        <button
+          class="px-2 font-bold hover:ring-2 hover:ring-inset hover:ring-black"
+          @click="deleteDirectory(state)"
+        >
+          <span class="px-0.5"> - </span>
+        </button>
+      </div>
     </div>
   </div>
 </template>
