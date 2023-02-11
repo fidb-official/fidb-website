dc552a264325e15ec6efcca0f98f68886caca9dd
Xie Yuheng <xyheme@gmail.com>
2023-02-11 19:57:03 +0800

    up

--- src/pages/manager/Table.ts
+++ src/pages/manager/Table.ts
@@ -15,19 +15,26 @@ export function tableColumnNames(dataset: Array<any>): Array<string> {
 
   columnNames = arrayDedup(columnNames)
 
-  // '@id' as the first column
-  if (columnNames.includes('@id')) {
-    columnNames.splice(columnNames.indexOf('@id'), 1)
-    columnNames.unshift('@id')
-  }
-
-  // '@revision' as the last column
-  if (columnNames.includes('@revision')) {
-    columnNames.splice(columnNames.indexOf('@revision'), 1)
-    columnNames.push('@revision')
-  }
+  startColumnName(columnNames, '@id')
+  endColumnName(columnNames, '@createdAt')
+  endColumnName(columnNames, '@updatedAt')
+  endColumnName(columnNames, '@revision')
 
   return columnNames
+}
+
+function startColumnName(columnNames: Array<string>, name: string): void {
+  if (columnNames.includes(name)) {
+    columnNames.splice(columnNames.indexOf(name), 1)
+    columnNames.unshift(name)
+  }
+}
+
+function endColumnName(columnNames: Array<string>, name: string): void {
+  if (columnNames.includes(name)) {
+    columnNames.splice(columnNames.indexOf(name), 1)
+    columnNames.push(name)
+  }
 }
 
 export function createTable(dataset: Array<any>): Table {
