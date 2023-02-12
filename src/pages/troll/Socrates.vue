e9e752ed63c986a6406868d1470df3b140a03ce7
Xie Yuheng <xyheme@gmail.com>
2023-02-13 04:06:47 +0800

    up

--- src/pages/troll/Socrates.vue
+++ src/pages/troll/Socrates.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div
-    class="my-3 mx-3 flex h-full flex-col divide-y-2 divide-stone-400 overflow-auto border-y-2 border-stone-400"
+    class="mx-3 flex h-full flex-col divide-y-2 divide-stone-400 overflow-auto border-y-2 border-stone-400"
   >
     <slot></slot>
   </div>
