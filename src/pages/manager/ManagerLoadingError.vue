92fecf77942221edcd1c5c68f6b97bea46e4fa97
Xie Yuheng <xyheme@gmail.com>
2023-02-16 20:12:18 +0800

    up

--- src/pages/manager/ManagerLoadingError.vue
+++ src/pages/manager/ManagerLoadingError.vue
@@ -5,7 +5,7 @@ defineProps<{
 </script>
 
 <template>
-  <div class="m-3 whitespace-pre border border-red-600 p-3 text-red-600">
+  <div class="m-3 whitespace-pre-wrap border border-red-600 p-3 text-red-600">
     {{ error.message }}
   </div>
 </template>
