8e4e95af035b2f7855c0e1f07795cf6cb1fe41bd
Xie Yuheng <xyheme@gmail.com>
2023-03-01 15:18:41 +0800

    use Login instead of SignIn

--- src/pages/routes.ts
+++ src/pages/routes.ts
@@ -3,14 +3,14 @@ import Docs from './docs/Docs.vue'
 import PageNotFound from './errors/PageNotFound.vue'
 import Home from './home/Home.vue'
 import Manager from './manager/Manager.vue'
-import ManagerSignIn from './manager/ManagerSignIn.vue'
+import ManagerLogin from './manager/ManagerLogin.vue'
 import Talks from './talks/Talks.vue'
 
 export const routes: Array<RouteRecordRaw> = [
   { path: '/', component: Home },
   { path: '/talks', component: Talks },
   { path: '/docs', component: Docs },
-  { path: '/manager', component: ManagerSignIn },
+  { path: '/manager', component: ManagerLogin },
   { path: '/manager/:url(.*)', component: Manager },
   { path: '/:pathMatch(.*)', component: PageNotFound },
 ]
